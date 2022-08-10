c3c2ca8acf12a6692b8118ec3f5516454a0a3a9d
Dmitry Melnikov <d.dash.mv@gmail.com>
2022-08-10 19:56:17 +0300

    Update

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 
+npm i -g jsonlint
+
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
