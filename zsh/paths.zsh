4b179cbe601a249e3149cfdd6d024586ce8056ad
Dmitry Melnikov <d.dash.mv@gmail.com>
2025-03-10 14:51:28 +0000

    Update

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -2,5 +2,4 @@ source $HOME/.cargo/env
 export PATH="$HOME/.fnm:$PATH"
 export PATH="/Users/dmelnikov/.local/bin:$PATH"
 export PATH="/Users/dmelnikov/Library/Python/3.12/bin:$PATH"
-. "/opt/homebrew/opt/asdf/libexec/asdf.sh"
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
